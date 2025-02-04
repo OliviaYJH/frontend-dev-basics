7d9104b023fc151397e7f01bf167c1814938b8d3
YuJeongHyun <0703olivia@naver.com>
2025-02-04 10:53:01 +0900

    js ex00 ~ ex04

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,3 +1,24 @@
 console.log("============[02.02] 표현식(Expression)");
+i = 4;
+2 + 2
+parseInt('4')
+Math.pow(2, 2)
+4
+
 console.log("============[05] 세미콜론(Semicolon)");
+a = 10; b = 20;
+
 console.log("============[06] 개행(Line Break)");
+k
+=
+10;
+console.log(k);
+
+
+console.log('hello world')
+
+
+function f() {
+    var x = 10;
+    return x * 10;
+}
