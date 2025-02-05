97c9acddd43e5c6329ef9df63210da9627e00763
YuJeongHyun <0703olivia@naver.com>
2025-02-05 11:22:53 +0900

    js ex09 ~ ex13

--- javascript-practices/basics/ex12.js
+++ javascript-practices/basics/ex12.js
@@ -1,5 +1,32 @@
 /* Lexical Scope */
+var f1 = function() {
+    var s = "hello world";
 
+    var inner = function() {
+        console.log(s);
+    }
+
+    inner();
+}
+
+f1();
 
 /* Closure is a function that has access to the outer function's scope in which it was created even after the outer function has finished executing. */
+var f2 = function() {
+    var s = "hello world";
 
+    var inner = function() {
+        console.log(s);
+    }
+
+    return inner;
+}
+
+var closure = f2();
+closure();
+
+function Component() {
+    var s = useState("10");
+
+    s = 20;
+}
