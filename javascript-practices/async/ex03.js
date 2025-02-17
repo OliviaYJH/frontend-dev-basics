9f465dd4666cf24919fcf1d2866e42ce1f328a66
YuJeongHyun <0703olivia@naver.com>
2025-02-17 17:08:13 +0900

    async

--- javascript-practices/async/ex03.js
+++ javascript-practices/async/ex03.js
@@ -1,3 +1,16 @@
 /*
     promise 지원 비동기 함수 사용 II: async ~ await
 */
+
+const {fn02} = require('./fns');
+
+// test: success
+const fn = async () => {
+    const result = await fn02('params~');
+    console.log(result);
+}
+
+fn('params~~');
+fn(null);
+
+console.log('wait...');
